dd3de61e0f617124380c18cb54d1bbc367978687
ebarmpas <56959801+ebarmpas@users.noreply.github.com>
2025-05-15 15:53:05 +0300

    Added logger (#10)
    Co-authored-by: vaggbarmpas@gmail.com <evbarmpas@seerc.org>

--- src/main/java/org/seerc/nebulous/rest/OntologyPostController.java
+++ src/main/java/org/seerc/nebulous/rest/OntologyPostController.java
@@ -20,67 +20,66 @@ import org.springframework.web.bind.annotation.RestController;
 public class OntologyPostController {
 	//TODO: add error checking and discuss non-volatile storage.
 	private OntologyDAO ontology = OntologyDAO.getInstance();
-	private DatabaseDAO db = DatabaseDAO.getInstance();
+//	private DatabaseDAO db = DatabaseDAO.getInstance();
 
 	
-	@PostMapping("/load/asset")
-	void loadAsset(@RequestBody String assetName) {
-		registerAsset(new RegisterAssetPostBody(assetName, Instant.now().getEpochSecond()));
-		db.getSLAClassAssertions(assetName).forEach(t -> 
-			ontology.getManipulator().createIndividual(t.get(0), t.get(1))
-		);
-		
-		db.getSLAObjectPropertyAssertions(assetName).forEach(t -> 
-			ontology.getManipulator().createObjectProperty(t.get(1), t.get(0), t.get(2))
-		);
-		db.getSLADataPropertyAssertions(assetName).forEach(t -> {
-			String v[] = t.get(2).split("^^");
-			ontology.getManipulator().createDataProperty(t.get(1), t.get(0), v[0], v[1]);
-		});
-	}
+//	@PostMapping("/load/asset")
+//	void loadAsset(@RequestBody String assetName) {
+//		registerAsset(new RegisterAssetPostBody(assetName, Instant.now().getEpochSecond()));
+//		db.getSLAClassAssertions(assetName).forEach(t -> 
+//			ontology.getManipulator().createIndividual(t.get(0), t.get(1))
+//		);
+//		
+//		db.getSLAObjectPropertyAssertions(assetName).forEach(t -> 
+//			ontology.getManipulator().createObjectProperty(t.get(1), t.get(0), t.get(2))
+//		);
+//		db.getSLADataPropertyAssertions(assetName).forEach(t -> {
+//			ontology.getManipulator().createDataProperty(t.get(1), t.get(0), t.get(2), t.get(3));
+//		});
+//	}
 	
-	@PostMapping("/register/asset")
-	void registerAsset(@RequestBody RegisterAssetPostBody postBody) {
-		ontology.registerAsset(postBody.getAssetName(), postBody.getTimestamp());
-		
-		if(ontology.numberOfAssets() >= 3) {
-			ontology.removeEarliestAsset();
-		}
-	}
+//	@PostMapping("/register/asset")
+//	void registerAsset(@RequestBody RegisterAssetPostBody postBody) {
+//		ontology.registerAsset(postBody.getAssetName(), postBody.getTimestamp());
+//		
+//		if(ontology.numberOfAssets() >= 3) {
+//			ontology.removeEarliestAsset();
+//		}
+//	}
 
 
 	@PostMapping("/create/individual")
 	void createIndividual(@RequestBody CreateIndividualPostBody postBody) {
 		ontology.getManipulator().createIndividual(postBody.getIndividualURI(), postBody.getClassURI());
-		db.createIndividual(postBody);
-//		Logger.post("Class Assertion", postBody.getIndividualURI(), postBody.getClassURI());
+//		db.createIndividual(postBody);
+		Logger.post("Class Assertion", postBody.getIndividualURI(), postBody.getClassURI());
 
 	}
 	@PostMapping("/create/individual/expression")
 	void createIndividualClassExpression(@RequestBody CreateIndividualPostBody postBody) {
 		ontology.getManipulator().createIndividualClassExpression(postBody.getIndividualURI(), postBody.getClassURI());
-//		Logger.post("Class Assertion", postBody.getIndividualURI(), postBody.getClassURI());
+		Logger.post("Class Assertion", postBody.getIndividualURI(), postBody.getClassURI());
 
 	}
 	@PostMapping("/create/class/expression")
 	void createClassDescriptionClass(@ RequestBody CreateClassExpressionClassPostBody postBody) {
 		ontology.getManipulator().createClassExpressionClass(postBody.getClassURI(), postBody.getClassExpression());
-//		Logger.post("Class Creation", postBody.getClassURI(), postBody.getClassExpression());
+		Logger.post("Class Creation", postBody.getClassURI(), postBody.getClassExpression());
 	}
 	
 	@PostMapping("/create/objectProperty")
 	void createObjectProperty(@RequestBody CreateObjectPropertyPostBody postBody) {
 		ontology.getManipulator().createObjectProperty(postBody.getObjectPropertyURI(), postBody.getDomainURI(), postBody.getRangeURI());
-		db.createObjectPropertyAssertion(postBody);
-		//		Logger.post("Object Property Assertion", "Object Property: " + postBody.getObjectPropertyURI(), "Domain: " + postBody.getDomainURI(), "Range: " + postBody.getRangeURI());
+//		db.createObjectPropertyAssertion(postBody);
+		Logger.post("Object Property Assertion", "Object Property: " + postBody.getObjectPropertyURI(), "Domain: " + postBody.getDomainURI(), "Range: " + postBody.getRangeURI());
 	}
 	
 	@PostMapping("/create/dataProperty")
 	void createDataProperty(@RequestBody CreateDataPropertyPostBody postBody) {
 		System.out.println(postBody);
 		ontology.getManipulator().createDataProperty(postBody.getDataPropertyURI(), postBody.getDomainURI(), postBody.getValue(), postBody.getType());
-		db.createDataPropertyAssertion(postBody);
-		//		Logger.post("Data Property Assertion", "Data Property: " + postBody.getDataPropertyURI(), "Domain: " + postBody.getDomainURI(), "Value: " + postBody.getValue());
+//		db.createDataPropertyAssertion(postBody);
+		Logger.post("Data Property Assertion", "Data Property: " + postBody.getDataPropertyURI(), "Domain: " + postBody.getDomainURI(), "Value: " + postBody.getValue());
 
 	}
 	
--- src/main/java/org/seerc/nebulous/sql/Database.java
+++ src/main/java/org/seerc/nebulous/sql/Database.java
@@ -75,12 +75,11 @@ public class Database {
 		
 			ResultSet rs = s.executeQuery();
 			while(rs.next())
-				res.add(List.of(rs.getString(1),  rs.getString(2), rs.getString(3)));
+				res.add(List.of(rs.getString(1),  rs.getString(2), rs.getString(3), rs.getString(4)));
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(res);
 		return res;
 	}
 	public void createIndividual(CreateIndividualPostBody postBody) {
