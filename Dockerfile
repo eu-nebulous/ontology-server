83a132baaa2c200c9255e92f5a1e404d5473d31d
ebarmpas <56959801+ebarmpas@users.noreply.github.com>
2025-12-05 12:36:01 +0200

    Updated deployment (#17)
    * Added endpoints to support class expression class definition
    
    * Initial changes to support complex SLA reasoning
    
    * Updated Controllers
    
    * Database persistence
    
    * Transformed it into being fully integrated with active-mq
    
    * Updated Dockerfile to two-step build
    
    * Fix
    
    ---------
    
    Co-authored-by: vaggbarmpas@gmail.com <evbarmpas@seerc.org>

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY pom.xml pom.xml
 
 RUN mvn package -Dmaven.test.skip
 
-WORKDIR /ontology-server/target
 
 #
 # Package stage
@@ -15,7 +14,6 @@ FROM docker.io/library/eclipse-temurin:17-jre
 COPY --from=build /ontology-server/target/nebulous-ont-0.0.1-SNAPSHOT.jar nebulous-ont.jar
 COPY nebulous.ttl nebulous.ttl
 
-
-CMD ["java", "-jar", "nebulous-ont.jar", "nebulous.ttl"]
+ENTRYPOINT ["java", "-jar", "nebulous-ont.jar", "nebulous.ttl"]
 
 EXPOSE 80
--- src/main/java/org/seerc/nebulous/rest/OntologyPostController.java
+++ src/main/java/org/seerc/nebulous/rest/OntologyPostController.java
@@ -53,23 +53,14 @@ public class OntologyPostController {
 	void createIndividual(@RequestBody CreateIndividualPostBody postBody) {
 		ontology.getManipulator().createIndividual(postBody.getIndividualURI(), postBody.getClassURI());
 //		db.createIndividual(postBody);
-//		Logger.post("Class Assertion", postBody.getIndividualURI(), postBody.getClassURI());
-
-	}
-	@PostMapping("/create/individual/expression")
-	void createIndividualClassExpression(@RequestBody CreateIndividualPostBody postBody) {
-		ontology.getManipulator().createIndividualClassExpression(postBody.getIndividualURI(), postBody.getClassURI());
-//		Logger.post("Class Assertion", postBody.getIndividualURI(), postBody.getClassURI());
-
-
-
-	}
-	@PostMapping("/create/individual/expression")
-	void createIndividualClassExpression(@RequestBody CreateIndividualPostBody postBody) {
-		ontology.getManipulator().createIndividualClassExpression(postBody.getIndividualURI(), postBody.getClassURI());
 		Logger.post("Class Assertion", postBody.getIndividualURI(), postBody.getClassURI());
 
 
+	}
+	@PostMapping("/create/individual/expression")
+	void createIndividualClassExpression(@RequestBody CreateIndividualPostBody postBody) {
+		ontology.getManipulator().createIndividualClassExpression(postBody.getIndividualURI(), postBody.getClassURI());
+//		Logger.post("Class Assertion", postBody.getIndividualURI(), postBody.getClassURI());
 	}
 	@PostMapping("/create/class/expression")
 	void createClassDescriptionClass(@ RequestBody CreateClassExpressionClassPostBody postBody) {
@@ -77,22 +68,12 @@ public class OntologyPostController {
 		Logger.post("Class Creation", postBody.getClassURI(), postBody.getClassExpression());
 
 	}
-	@PostMapping("/create/class/expression")
-	void createClassDescriptionClass(@ RequestBody CreateClassExpressionClassPostBody postBody) {
-		ontology.getManipulator().createClassExpressionClass(postBody.getClassURI(), postBody.getClassExpression());
-//		Logger.post("Class Creation", postBody.getClassURI(), postBody.getClassExpression());
-	}
-	@PostMapping("/create/class/expression")
-	void createClassDescriptionClass(@ RequestBody CreateClassExpressionClassPostBody postBody) {
-		ontology.getManipulator().createClassExpressionClass(postBody.getClassURI(), postBody.getClassExpression());
-//		Logger.post("Class Creation", postBody.getClassURI(), postBody.getClassExpression());
-	}
 	
 	@PostMapping("/create/objectProperty")
 	void createObjectProperty(@RequestBody CreateObjectPropertyPostBody postBody) {
 		ontology.getManipulator().createObjectProperty(postBody.getObjectPropertyURI(), postBody.getDomainURI(), postBody.getRangeURI());
 //		db.createObjectPropertyAssertion(postBody);
-		//		Logger.post("Object Property Assertion", "Object Property: " + postBody.getObjectPropertyURI(), "Domain: " + postBody.getDomainURI(), "Range: " + postBody.getRangeURI());
+		Logger.post("Object Property Assertion", "Object Property: " + postBody.getObjectPropertyURI(), "Domain: " + postBody.getDomainURI(), "Range: " + postBody.getRangeURI());
 	}
 	
 	@PostMapping("/create/dataProperty")
